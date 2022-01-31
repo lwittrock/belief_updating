6ff8dd94dbc215349a8addbe99652a08f2d992c1
larwitt <larwitt@gmx.de>
2022-01-31 10:37:08 +0100

    return link

--- settings.py
+++ settings.py
@@ -41,3 +41,15 @@ ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 DEMO_PAGE_INTRO_HTML = """ """
 
 SECRET_KEY = '7277142838663'
+
+
+ROOMS = [
+    dict(
+        name='test_room',
+        display_name='Test Room name',
+    ),
+    dict(
+        name='another_room',
+        display_name='Test Room name 2',
+    ),
+]
